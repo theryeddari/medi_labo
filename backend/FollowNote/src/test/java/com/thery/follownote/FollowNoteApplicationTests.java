196e56164ad0db3a544ecd776c5c2f70510abd84
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-27 14:29:01 +0200

    create the skeleton of the first backend FollowNote module  (with the child POM and Dockerfile)

---
+++ backend/FollowNote/src/test/java/com/thery/follownote/FollowNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thery.follownote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
