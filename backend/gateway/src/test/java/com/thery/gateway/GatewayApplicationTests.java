a651a4b35a7fb3ee5fd91f4d25d76ceec028a463
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-27 17:11:26 +0200

    configure compose.yalm with all modules for Docker and initialization scripts for databases.

--- backend/gateway/src/test/java/com/thery/gateway/GatewayApplicationTests.java
+++ backend/gateway/src/test/java/com/thery/gateway/GatewayApplicationTests.java
@@ -1,14 +1,10 @@
 package com.thery.gateway;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GatewayApplicationTests {
-
-    @Value("${angular.url}")
-    private String angular;
 
     @Test
     void contextLoads() {
