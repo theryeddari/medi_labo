d604555e8da611b91e1d96712023185e4b5ffc12
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-09-01 04:06:28 +0200

    mistake del script

---
+++ init_scripts/mysql/01_init_patient_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS patient
+(
+    patientId INT AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(45) NOT NULL,
+    username  VARCHAR(45) NOT NULL,
+    birthdate DATETIME    NOT NULL,
+    gender    VARCHAR(45) NOT NULL,
+    phone     VARCHAR(15),
+    adresse   VARCHAR(100)
+);
