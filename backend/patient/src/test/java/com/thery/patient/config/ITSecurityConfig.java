b014497309a818eb4908b208fbfecaf92cc004f8
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-09-01 03:43:26 +0200

    modify mistake auth test mockUser

--- backend/patient/src/test/java/com/thery/patient/config/ITSecurityConfig.java
+++ backend/patient/src/test/java/com/thery/patient/config/ITSecurityConfig.java
@@ -2,13 +2,13 @@ package com.thery.patient.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Base64;
 
 
 @SpringBootTest
@@ -18,21 +18,19 @@ public class ITSecurityConfig {
     @Autowired
     private MockMvc mockMvc;
 
-    @Value("${ROUTE_PATIENT}")
-    private String ROUTE_PATIENT;
 
     //WithMockUser password by default its password
     @Test
-    @WithMockUser(username = "doctor")
     public void testAuthenticationAuthorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/clientele"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/clientele")
+                        .header("Authorization", "Basic " + Base64.getEncoder().encodeToString("doctor:password".getBytes())))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
-    @WithMockUser(username = "doctor", password = "wrong")
     public void testAuthenticationUnauthorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/clientele"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/clientele")
+                        .header("Authorization", "Basic " + Base64.getEncoder().encodeToString("user:password".getBytes())))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
--- backend/patient/src/test/java/com/thery/patient/controller/ITPatientController.java
+++ backend/patient/src/test/java/com/thery/patient/controller/ITPatientController.java
@@ -12,8 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
+import java.util.Base64;
 import java.util.Objects;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -32,24 +34,25 @@ public class ITPatientController {
     private ObjectMapper objectMapper;
 
     @Test
-    @WithMockUser(username = "doctor")
     public void testGetClientele_Success() throws Exception {
 
         mockMvc.perform(get("/clientele")
-                        .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Basic " + Base64.getEncoder().encodeToString("doctor:password".getBytes())))
+                .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(status().isOk())
                 .andExpect(response ->
                         Assertions.assertTrue(Objects.requireNonNull(response.getResponse().getContentAsString()).contains("TestNone")));
     }
 
     @Test
-    @WithMockUser(username = "doctor")
     public void testGetPatient_Success() throws Exception {
 
         mockMvc.perform(get("/clientele")
                         .contentType(MediaType.APPLICATION_JSON)
                         .param("patientId", "1")
-                )
+                        .header("Authorization", "Basic " + Base64.getEncoder().encodeToString("doctor:password".getBytes())))
+                .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(status().isOk())
                 .andExpect(response ->
                         Assertions.assertTrue(Objects.requireNonNull(response.getResponse().getContentAsString()).contains("TestNone")));
@@ -64,7 +67,8 @@ public class ITPatientController {
         String jsonResponse = mockMvc.perform(post("/patient")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(patientRequest))
-                )
+                        .header("Authorization", "Basic " + Base64.getEncoder().encodeToString("doctor:password".getBytes())))
+                .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
